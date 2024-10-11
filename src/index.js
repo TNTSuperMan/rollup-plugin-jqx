75d2d74ee7feaa3a22886b236650cc59e4ff304c
TNTSuperMan <tntsuperga@gmail.com>
2024-10-11 12:09:35 +0900

    solved

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import { JSDOM } from "jsdom"
 function convertElm(elm){
     //Tag, attr, content
     let arr = [elm.tagName,[],[]]
-    elm.childNodes.forEach(e=>
-        arr[2].push(e.nodeType == 1 ? convertElm(e) : e.nodeValue )
-    )
+    elm.childNodes.forEach(e=>{
+        if(e.nodeType == 3){
+            if(e.nodeValue.trim())
+                arr[2].push(e.nodeValue)
+        }else{
+            arr[2].push(convertElm(e))
+        }
+    })
     for(let i = 0;i < elm.attributes.length;i++)
         arr[1].push([
             elm.attributes.item(i)?.nodeName,
@@ -19,12 +24,12 @@ function genFromSrc(src){
     if(!template) throw new Error("Not found template element")
     return `//By TNTSuperMan's rollup-plugin-jqx
 import $ from "jqx";
-export default function (props) => {
+export default function(props){
     //Short element generator(SEG)
     const h=e=>{let t=document.createElement(e[0]);return e[1].forEach((e=>e[0]?t.setAttribute(e[0],e[1]??""):0)),e[2].forEach((e=>t.appendChild(Array.isArray(e)?h(e):new Text(e??"")))),t};
-
+    
     //Generate base element by SEG
-    let $this = $(h(${JSON.stringify(convertElm(template))});
+    let $this = $(h( ${JSON.stringify(convertElm(template))} ));
     //Setup script
 ${script?script.innerHTML:""}
     //Return this
@@ -36,11 +41,11 @@ export default function(){
         name: "rollup-plugin-jqx",
         transform(src, id){
             if(/\.jqx$/.test(id)){
-                let code = genFromSrc(src)
-                console.log(typeof code)
-                return `${code}`
-                console.log(code)
-                return {code,map:null}
+                let a = {
+                    code: genFromSrc(src),
+                    map: null
+                };
+                return a;
             }
         }
     }
